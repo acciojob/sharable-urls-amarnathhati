368d32d60f30d20ba77ad490ce82e9428d20384f
amarnathhati <119184772+amarnathhati@users.noreply.github.com>
2023-08-01 09:51:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+document.getElementById("urlForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent form submission to stop page refresh
+      updateURL();
+    });
+
+    function updateURL() {
+      const name = document.getElementById("name").value;
+      const year = document.getElementById("year").value;
+
+      const url = `https://localhost:8080/?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+      document.getElementById("url").textContent = url;
+    }
